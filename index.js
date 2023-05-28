34a4f1eab1b4d3a530637ce328deabd9b8c4ff3e
kadekframa <dframa19@gmail.com>
2023-05-28 11:49:20 +0800

    Added new proxy

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-import cors_proxy from "cors-anywhere";
+import express from "express";
 
-const host = process.env.HOST || "0.0.0.0";
+const app = express();
 
-// Listen on a specific port via the PORT environment variable
-const port = process.env.PORT || 8080;
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
 
-cors_proxy
-  .createServer({
-    originWhitelist: [], // Allow all origins
-  })
-  .listen(port, host, function () {
-    console.log("Running CORS Anywhere on " + host + ":" + port);
-  });
+  next();
+});
+
+app.get("/users", async (_req, res) => {
+  const url = "https://data.bmkg.go.id/DataMKG/TEWS/gempadirasakan.json";
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Error! status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return res.json(result);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+const port = 4000;
+
+app.listen(port, () =>
+  console.log(`Server running on http://localhost:${port}`)
+);
